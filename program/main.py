53ebcae00627c9eea5941bdb9eefb555ae930665
Vadim <vadim.vergasov2003@gmail.com>
2018-09-22 13:10:16 +0300

    No .kv files, because can't make localization with them

--- program/main.py
+++ program/main.py
@@ -14,8 +14,11 @@ from kivy.animation import Animation
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 from kivy.uix.image import Image
-from kivy.uix.behaviors import ButtonBehavior  
+from kivy.uix.behaviors import ButtonBehavior
+
+import gettext
 
 Config.set('kivy', 'log_dir', os.getcwd()+"\\log")
 Config.set('kivy', 'desktop', 1)
@@ -26,14 +29,26 @@ Config.write()
 class ImageButton(ButtonBehavior, Image):
     pass
 
-class Screen(FloatLayout):
-    pass
-
 class MaketestApp(App):
     title = 'Making test'
 
     def build(self):
-        return Screen()
+        layout = FloatLayout(size=(300, 300))
+        button = Button(text='Language/Мова/Язык', size_hint=(.5, .1), pos_hint={'x': .25, 'y': .5})
+        dropdown = DropDown()
+        lng1 = Button(text='English', size_hint_y=None, height=48)
+        lng1.bind(on_release=lambda lng1: dropdown.select(lng1.text))
+        lng2 = Button(text='Беларуская', size_hint_y=None, height=48)
+        lng2.bind(on_release=lambda lng2: dropdown.select(lng2.text))
+        lng3 = Button(text='Русский', size_hint_y=None, height=48)
+        lng3.bind(on_release=lambda lng3: dropdown.select(lng3.text))
+        dropdown.add_widget(lng1)
+        dropdown.add_widget(lng2)
+        dropdown.add_widget(lng3)
+        button.bind(on_release=dropdown.open)
+        dropdown.bind(on_select=lambda instance, x: setattr(button, 'text', x))
+        layout.add_widget(button)
+        return layout
 
     def on_pause(self):
         return True
